fb6e9c68b2bc8c118c4f7a9b80a216dd17f7ba3f
stOne <stoneskull@gmail.com>
2022-04-27 13:26:19 +1000

    testy NPCs

--- tred.py
+++ tred.py
@@ -1,10 +1,11 @@
 from threading import Thread
 from time import sleep
+from random import randint
 
 
 class Tred(Thread):
     # override the run function
     def run(self):
-        self.value = 23
+        self.value = randint(1, 23)
         sleep(2)
         print('..This is coming from thread', self)
